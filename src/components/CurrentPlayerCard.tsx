6ff9ac4bdb4285c8fb694a3e7698047cb4225087
lpoultersky <lpoultersky@gmail.com>
2023-02-06 21:33:02 +0000

    use next photo

--- src/components/CurrentPlayerCard.tsx
+++ src/components/CurrentPlayerCard.tsx
@@ -1,11 +1,19 @@
 import type { Player } from "../types";
+import Image from "next/image";
 
 export function CurrentPlayerCard({ player }: { player: Player }) {
+  const loader = ({ src }: { src: string }) => {
+    return `https://resources.premierleague.com/premierleague/photos/players/110x140/p${src}.png`;
+  };
+
   return (
     <div className="flex max-w-xs flex-col items-center justify-center overflow-hidden rounded bg-blue-300 pt-3 pl-6 pr-6 shadow-sm shadow-orange-700">
-      <img
-        className="max-h-40 rounded-full"
-        src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player?.playerId}.png`}
+      <Image
+        src={`${player?.playerId}`}
+        loader={loader}
+        width={110}
+        height={140}
+        className="rounded-full"
         alt="a nice face"
       />
       <p className="m-3 text-xl text-white">{player?.web_name}</p>
