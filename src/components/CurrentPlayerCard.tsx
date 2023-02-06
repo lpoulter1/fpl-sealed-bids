a829996571e72ef4f9046d978c6f4e3171a9d7d5
lpoultersky <lpoultersky@gmail.com>
2023-02-06 14:49:55 +0000

    quick style changes. make input number

--- src/components/CurrentPlayerCard.tsx
+++ src/components/CurrentPlayerCard.tsx
@@ -2,7 +2,7 @@ import type { Player } from "../types";
 
 export function CurrentPlayerCard({ player }: { player: Player }) {
   return (
-    <div className="flex max-w-xs flex-col items-center justify-center overflow-hidden rounded bg-blue-300 pt-3 pl-6 pr-6 shadow-lg">
+    <div className="flex max-w-xs flex-col items-center justify-center overflow-hidden rounded bg-blue-300 pt-3 pl-6 pr-6 shadow-sm shadow-orange-700">
       <img
         className="max-h-40 rounded-full"
         src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player?.playerId}.png`}
