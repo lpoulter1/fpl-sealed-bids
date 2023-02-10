fd6125218d2fff390b04f429d631ef232e8b4f75
lpoultersky <lpoultersky@gmail.com>
2023-02-10 23:14:44 +0000

    render the same data but with React Table

--- src/pages/players.tsx
+++ src/pages/players.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { useTable } from "react-table";
+
 type Player = {
   id: number;
   first_name: string;
@@ -93,122 +96,176 @@ function getPosition(element_type: number) {
 }
 
 function Page({ players }: Props) {
+  // all columns
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Player",
+        accessor: "web_name",
+      },
+      {
+        Header: "Position",
+        accessor: "element_type",
+        Cell: ({ value }) => getPosition(value),
+      },
+      {
+        Header: "Team",
+        accessor: "team",
+      },
+      {
+        Header: "Total points",
+        accessor: "total_points",
+      },
+      {
+        Header: "News",
+        accessor: "news",
+      },
+      {
+        Header: "Chance of playing this round",
+        accessor: "chance_of_playing_this_round",
+      },
+      {
+        Header: "Chance of playing next round",
+        accessor: "chance_of_playing_next_round",
+      },
+      {
+        Header: "Ep this",
+        accessor: "ep_this",
+      },
+      {
+        Header: "Ep next",
+        accessor: "ep_next",
+      },
+      {
+        Header: "Value form",
+        accessor: "value_form",
+      },
+
+      {
+        Header: "Value season",
+        accessor: "value_season",
+      },
+      {
+        Header: "Selected by percent",
+        accessor: "selected_by_percent",
+      },
+      {
+        Header: "Form",
+        accessor: "form",
+      },
+      {
+        Header: "Event points",
+        accessor: "event_points",
+      },
+      {
+        Header: "Points per game",
+        accessor: "points_per_game",
+      },
+      {
+        Header: "Starts",
+        accessor: "starts",
+      },
+      {
+        Header: "Minutes",
+        accessor: "minutes",
+      },
+      {
+        Header: "Goals scored",
+        accessor: "goals_scored",
+      },
+      {
+        Header: "Assists",
+        accessor: "assists",
+      },
+      {
+        Header: "Clean sheets",
+        accessor: "clean_sheets",
+      },
+      { Header: "Clean sheets p/90", accessor: "clean_sheets_per_90" },
+      { Header: "Goals conceded", accessor: "goals_conceded" },
+      { Header: "Own goals", accessor: "own_goals" },
+      { Header: "Penalties saved", accessor: "penalties_saved" },
+      { Header: "Penalties missed", accessor: "penalties_missed" },
+      { Header: "Yellow cards", accessor: "yellow_cards" },
+      { Header: "Red cards", accessor: "red_cards" },
+      { Header: "Saves", accessor: "saves" },
+      { Header: "Bonus", accessor: "bonus" },
+      { Header: "Bps", accessor: "bps" },
+
+      { Header: "Influence", accessor: "influence" },
+      { Header: "Creativity", accessor: "creativity" },
+      { Header: "Threat", accessor: "threat" },
+      { Header: "Ict index</t", accessor: "ict_index" },
+
+      {
+        Header: "expected_goals_conceded",
+        accessor: "expected_goals_conceded",
+      },
+      {
+        Header: "corners_and_indirect_freekicks_text",
+        accessor: "corners_and_indirect_freekicks_text",
+      },
+      {
+        Header: "corners_and_indirect_freekicks_order",
+        accessor: "corners_and_indirect_freekicks_order",
+      },
+      {
+        Header: "direct_freekicks_order",
+        accessor: "direct_freekicks_order",
+      },
+      { Header: "direct_freekicks_text", accessor: "direct_freekicks_text" },
+      { Header: "expected_assists", accessor: "expected_assists" },
+      {
+        Header: "expected_assists_per_90",
+        accessor: "expected_assists_per_90",
+      },
+      {
+        Header: "expected_goal_involvements",
+        accessor: "expected_goal_involvements",
+      },
+      {
+        Header: "expected_goal_involvements_per_90",
+        accessor: "expected_goal_involvements_per_90",
+      },
+      { Header: "expected_goals", accessor: "expected_goals" },
+    ],
+    []
+  );
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data: players,
+    });
+
   return (
-    <table className="table-auto  text-center">
-      <thead className="bg-gray-50 text-xs border-b">
-        <tr>
-          <th>Player</th>
-          <th>Position</th>
-          <th>Team</th>
-          <th>Total points</th>
-          
-          <th>News</th>
-          <th>Chance of playing this round</th>
-          <th>Chance of playing next round</th>
-          <th>Ep this</th>
-          <th>Ep next</th>
-
-          <th>Value form</th>
-          <th>Value season</th>
-          <th>Selected by percent</th>
-          <th>Form</th>
-
-          <th>Event points</th>
-          <th>Points per game</th>
-
-          <th>Starts</th>
-          <th>Minutes</th>
-
-          <th>Goals scored</th>
-          <th>Assists</th>
-          <th>Clean sheets</th>
-          <th>Clean sheets p/90</th>
-          <th>Goals conceded</th>
-          <th>Own goals</th>
-          <th>Penalties saved</th>
-          <th>Penalties missed</th>
-          <th>Yellow cards</th>
-          <th>Red cards</th>
-          <th>Saves</th>
-          <th>Bonus</th>
-          <th>Bps</th>
-
-          <th>Influence</th>
-          <th>Creativity</th>
-          <th>Threat</th>
-          <th>Ict index</th>   
-
-          <th>expected_goals_conceded</th>            
-          <th>corners_and_indirect_freekicks_text</th>            
-          <th>corners_and_indirect_freekicks_order</th>            
-          <th>direct_freekicks_order</th>   
-          <th>direct_freekicks_order</th>
-          <th>direct_freekicks_text</th>
-          <th>expected_assists</th>
-          <th>expected_assists_per_90</th>
-          <th>expected_goal_involvements</th>
-          <th>expected_goal_involvements_per_90</th>
-          <th>expected_goals</th>
-        </tr>
-      </thead>
-      <tbody>
-        {players.map((player) => (
-          <tr key={player.id}>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.first_name} {player.second_name} ({player.web_name})</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.team}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{getPosition(player.element_type)}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.total_points}</td>
-
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.news}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.chance_of_playing_this_round}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.chance_of_playing_next_round}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.ep_this}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.ep_next}</td>
-
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.value_form}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.value_season}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.selected_by_percent}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.form}</td>
-            
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.event_points}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.points_per_game}</td>
-         
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.starts}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.minutes}</td>
-
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.goals_scored}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.assists}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.clean_sheets}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.clean_sheets_per_90}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.goals_conceded}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.own_goals}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.penalties_saved}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.penalties_missed}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.yellow_cards}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.red_cards}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.saves}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.bonus}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.bps}</td>
-
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.influence}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.creativity}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.threat}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.ict_index}</td>    
-       
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.expected_goals_conceded}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.corners_and_indirect_freekicks_text}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.corners_and_indirect_freekicks_order}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.direct_freekicks_order}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.direct_freekicks_order}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.direct_freekicks_text}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.expected_assists}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.expected_assists_per_90}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.expected_goal_involvements}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.expected_goal_involvements_per_90}</td>
-            <td className="px-6 py-3 border-b text-xs text-grey-900">{player.expected_goals}</td>
-            
+    <table {...getTableProps()} className="table-auto  text-center">
+      <thead className="border-b bg-gray-50 text-xs">
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
           </tr>
         ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                    className="text-grey-900 border-b px-6 py-3 text-xs"
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
